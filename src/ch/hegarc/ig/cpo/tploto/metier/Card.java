f43b90ea0bb4c81f64b094c30a61050d2f80bf69
DraftDario <dario.mosca@IG14-MOSCAD.intra.eiaj.ch>
2016-05-23 12:18:18 +0200

    structure_checkLines
    Structure des boucles du checkLines()

--- src/ch/hegarc/ig/cpo/tploto/metier/Card.java
+++ src/ch/hegarc/ig/cpo/tploto/metier/Card.java
@@ -5,7 +5,7 @@ package ch.hegarc.ig.cpo.tploto.metier;
  * Auteur : Dario, Dimitri, Dylan, Sébastien
  */
 public class Card {
-    
+
     private Integer number;
     private Integer[][] contents;
 
@@ -15,11 +15,24 @@ public class Card {
     public Integer getNumber() {
         return number;
     }
-    
-    protected boolean checkLines(){
-        return true;
+
+    protected boolean checkLines() {
+        int count = 0;
+        boolean lineQuine = false;
+        int line = 0;
+
+        do {
+            for (int j = 0; j < 5; j++) {
+                //mettre le traitement
+                if (count == 5) {
+                    lineQuine = true;
+                }
+            }
+            line = line++;
+            count = 0;
+        } while (line < 3 && lineQuine == false);
+
+        return lineQuine;
     }
-    
-    
-    
+
 }
