a65390c4ecd7b27e26dd7fb9f31fb053abd91a66
Dim8672 <dimitri.mella@he-arc.ch>
2016-05-23 11:50:34 +0200

    AddFunction

--- src/ch/hegarc/ig/cpo/tploto/metier/Loto.java
+++ src/ch/hegarc/ig/cpo/tploto/metier/Loto.java
@@ -7,36 +7,38 @@ import java.util.List;
  * Auteur : Dario, Dimitri, Dylan, Sébastien
  */
 public class Loto {
+
     private List<Integer> nombresTires;
-    private List<Card> cardsList; 
+    private List<Card> cardsList;
 
     public Loto() {
     }
-    
-    public boolean ControlerQuine(Integer cardNumber){
+
+    public boolean controlerQuine(Integer cardNumber) {
         Card card = new Card();
         if (card = this.getCardByNumber(cardNumber)) {
-            
+
         }
     }
-    
-//    private Carte getCarteByNumber(Integer numero){
-//        
-//    }
+
+    private Card getCarteByNumber(Integer numero) {
+        int cardNumber = 0;
+        if (carteExist(numero)) {
+            for (int i = 0; i < cardsList.size(); i++) {
+                if (cardsList.get(i).getNumero() == numero) {
+                    cardNumber = i;
+                }
+            }           
+        }
+        return cardsList.get(cardNumber);
+    }
 
     public List<Integer> getNombresTires() {
         return nombresTires;
     }
-    
-    public boolean CardExist(Integer  cardNumber){
-        
-        for (int i=0;i<cardsList.size();i++){
-            if(cardsList.get(i).getNumero() == cardNumber){
-                return true;
-            }
-        }
-        return false;
+
+    public boolean carteExist(Integer numeroCarte) {
+        return true;
     }
-    
-    
+
 }
