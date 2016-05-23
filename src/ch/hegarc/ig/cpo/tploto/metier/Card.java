f556b06c99781595a37f7699568963a3288cb059
DraftDario <dario.mosca@IG14-MOSCAD.intra.eiaj.ch>
2016-05-23 12:30:41 +0200

    checkLine_in_curse
    En travaux

--- src/ch/hegarc/ig/cpo/tploto/metier/Card.java
+++ src/ch/hegarc/ig/cpo/tploto/metier/Card.java
@@ -1,5 +1,7 @@
 package ch.hegarc.ig.cpo.tploto.metier;
 
+import java.util.List;
+
 /**
  *
  * Auteur : Dario, Dimitri, Dylan, Sébastien
@@ -8,8 +10,10 @@ public class Card {
 
     private Integer number;
     private Integer[][] contents;
+    private Loto loto;
 
     public Card() {
+        loto = new Loto();
     }
 
     public Integer getNumber() {
@@ -20,17 +24,23 @@ public class Card {
         int count = 0;
         boolean lineQuine = false;
         int line = 0;
+        List<Integer> listNumbers = loto.getPulledNumbers();
 
         do {
             for (int j = 0; j < 5; j++) {
-                //mettre le traitement
+                
+                for (Integer nb = 0; nb < listNumbers.size(); nb++){
+                   // TESTER SI IF nb = contents[line][j];
+                }
+                
+
                 if (count == 5) {
                     lineQuine = true;
                 }
             }
             line = line++;
             count = 0;
-        } while (line < 3 && lineQuine == false);
+        } while (line < 3 && !lineQuine);
 
         return lineQuine;
     }
